54ca89a0de627a77e77444b67bb95e0571c5aff5
Szili Péter <peter.szili@kh.hu>
2020-03-06 14:19:51 +0100

    design implement

--- public/scripts/heart.js
+++ public/scripts/heart.js
@@ -10,4 +10,43 @@ $( document ).ready(function() {
     $ (".album").click(function() {
         document.location.href = $(this).attr("ref");
     })
+
+
+    $ (".grid-item").click(function() {
+        if ($(this)[0].hasAttribute("ref") ) {
+            document.location.href = $(this).attr("ref");
+        }
+    })
+
+    $ (".rounded-button").click(function() {
+        if ($(this)[0].hasAttribute("ref") ) {
+            document.location.href = $(this).attr("ref");
+        }
+    })
 });
+
+var uploadModal = document.getElementById("uploadModal");
+var uploadButton = document.getElementById("uploadButton");
+
+uploadButton.onclick = function() {
+    uploadModal.style.display = "block";
+}
+
+window.onclick = function(event) {
+  if (event.target == uploadModal) {
+    uploadModal.style.display = "none";
+  }
+}
+
+function readURL(input) {
+    if (input.files && input.files[0]) {
+        var reader = new FileReader();
+
+        reader.onload = function (e) {
+            $('#image-presenter')
+                .attr('src', e.target.result);
+        };
+
+        reader.readAsDataURL(input.files[0]);
+    }
+}
--- routes/album.js
+++ routes/album.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+var images = [{ id: 1,
+  imageUri: null,
+}, { id: 2,
+  imageUri: null,
+},{ id: 3,
+  imageUri: null,
+},{ id: 4,
+  imageUri: null,
+}];
+
 router.get('/:id', function(req, res, next) {
-  res.render('album', { title: req.params.id });
+  res.render('album', { images: images });
 });
 
 module.exports = router;
