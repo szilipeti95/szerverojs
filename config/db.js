17cf88da257e9edc30a1e29f311948d850b52dc0
Szili Péter <peter.szili@kh.hu>
2020-05-07 18:12:46 +0200

    update db.js

--- config/db.js
+++ config/db.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/jji6wk', { useNewUrlParser: true });
+mongoose.connect('mongodb://localhost/jji6wk', { useUnifiedTopology: true, useNewUrlParser: true }, function(err) {
+    if (err) console.log(err);
+});
 
 module.exports = mongoose;
